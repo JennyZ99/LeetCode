06928a63e6a0a38a998b01a2c8a47cb38b319ba2
JennyZ99 <101200278+JennyZ99@users.noreply.github.com>
2022-03-15 11:00:18 -0400

    Create  solution.py

---
+++ tag:hashtable or set/easy/350. Intersection of Two Arrays II/solution.py
@@ -0,0 +1,16 @@
+"""
+pros: 
+this solution is short and easy to understand;
+space: O(1)
+cons: 
+time complexity: O(m*n) in the worst case;
+**note: m is the size of nums1, and n is the size of nums2
+"""
+
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        intersect = []
+        for val in nums1:
+            if val in nums2:
+                intersect.append(val)
+        return intersect
