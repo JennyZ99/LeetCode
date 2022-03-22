451a4ab87bda5f40e395cba8beb825f9f12c5ddb
Jenny Zhang <101200278+JennyZ99@users.noreply.github.com>
2022-03-22 10:36:22 -0400

    Create solution.py

---
+++ tag::greedy/easy/2190. Most Frequent Number Following Key In an Array/solution.py
@@ -0,0 +1,26 @@
+"""
+runtime: O(n)
+"""
+
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        dic = {}
+        for bill in bills:
+            if bill in dic:
+                dic[bill] = dic[bill]+1
+            else:
+                dic[bill] = 1
+            if bill == 10:
+                if 5 in dic and dic[5]>=1:
+                    dic[5] = dic[5]-1
+                else: 
+                    return False
+            elif bill == 20:
+                if 10 in dic and dic[10]>=1 and 5 in dic and dic[5]>=1:
+                    dic[10] = dic[10]-1
+                    dic[5] = dic[5]-1
+                elif 5 in dic and dic[5]>=3:
+                    dic[5] = dic[5]-3
+                else:
+                    return False
+        return True
